3857a058cd6b33ef9aa7f523e6ed5e207a3ab3c8
angarciama <angarciama@unal.edu.co>
2024-03-03 21:24:09 -0500

    [Advance] -GeneralUserCard: dropdown

--- component/UsersCard/GeneralUserCard.vue
+++ component/UsersCard/GeneralUserCard.vue
@@ -1,24 +1,40 @@
 <template>
-  <div v-for="user in users" :key="user.id" class="general-user-card-container" @click="redirectToUser(user.uid)">
-    <img :src="user.avatar" alt="Avatar" class="general-user-card-img">
-    <section class="general-user-card-section">
-      <div>{{ user.last_name }}, {{ user.first_name }}</div>
-      <div>{{ user.email }}</div>
-      <div>{{ user.username }}</div>
-    </section>
+  <div>
+    <select v-model="selectedCount" @change="handleChange">
+      <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
+    </select>
+
+    <div v-for="user in showUsers" :key="user.id" class="general-user-card-container" @click="redirectToUser(user.uid)">
+      <img :src="user.avatar" alt="Avatar" class="general-user-card-img">
+      <section class="general-user-card-section">
+        <div>{{ user.last_name }}, {{ user.first_name }}</div>
+        <div>{{ user.email }}</div>
+        <div>{{ user.username }}</div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import GeneralUserModel from '~/api/models/GeneralUserModel'
-import {navigateTo} from "#app/composables/router";
+import { navigateTo } from "#app/composables/router";
 
-defineProps({
+const props = defineProps({
   users: {
     type: Array as () => GeneralUserModel[],
     required: true
   }
 })
+
+const selectedCount = ref<number>(5)
+const options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const showUsers = ref(props.users.slice(0, selectedCount.value))
+
+showUsers.value = props.users.slice(0, selectedCount.value)
+const handleChange = () => {
+  showUsers.value = props.users.slice(0, selectedCount.value)
+}
 
 const redirectToUser = async (userId: string) => {
   await navigateTo(`/user/${userId}`)
