355d40b2d69af4e171d2ba357e81ae34a7f5d170
angarciama <angarciama@unal.edu.co>
2024-03-03 20:58:22 -0500

    [Advance] -se configura variable de entorno -uso de variable de entorno en el controlador

--- api/controllers/users.controller.ts
+++ api/controllers/users.controller.ts
@@ -2,10 +2,13 @@ import GeneralUserModel from '~/api/models/GeneralUserModel'
 
 export const getUsers = async (): Promise<GeneralUserModel[]> => {
     try {
-        const response = await fetch('https://random-data-api.com/api/v2/users?size=10&response_type=json')
+        const config = useRuntimeConfig()
+
+        const response = await fetch(config.public.apiBackUrl)
         if (!response.ok) {
             throw new Error('Error fetching users')
         }
+
         return await response.json()
     } catch (error) {
         console.error('Error fetching users:', error)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
+  runtimeConfig: {
+    public: {
+      apiBackUrl: process.env.API_BACK_URL ?? 'https://random-data-api.com/api/v2/users?size=10&response_type=json'
+    }
+  }
 })
