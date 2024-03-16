af156580f036fb7340a08612affa128e3dd9f564
andersonpuntored <anderson.garcia@puntored.co>
2024-03-16 15:21:30 -0500

    [Advance] -Layout: se genera componente de maquetacion -GeneralUserCard & UserInformationCard: consumo de Layout y ajustes CSS

--- component/UsersCard/GeneralUserCard.vue
+++ component/UsersCard/GeneralUserCard.vue
@@ -1,24 +1,29 @@
 <template>
-  <div>
-    <select v-model="selectedCount">
-      <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
-    </select>
-
-    <div v-for="user in displayedUsers" :key="user.id" class="general-user-card-container" @click="redirectToUser(user.uid)">
-      <img :src="user.avatar" alt="Avatar" class="general-user-card-img">
-      <section class="general-user-card-section">
-        <div>{{ user.last_name }}, {{ user.first_name }}</div>
-        <div>{{ user.email }}</div>
-        <div>{{ user.username }}</div>
-      </section>
-    </div>
-  </div>
+  <Layout>
+    <template #mainSlot>
+      <div class="general-user-card-select-container">
+        <div>Usuarios a listar</div>
+        <select v-model="selectedCount" class="general-user-card-select">
+          <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
+        </select>
+      </div>
+      <div v-for="user in displayedUsers" :key="user.id" class="general-user-card-container" @click="redirectToUser(user.uid)">
+        <img :src="user.avatar" alt="Avatar" class="general-user-card-img">
+        <section class="general-user-card-section">
+          <div>{{ user.last_name }}, {{ user.first_name }}</div>
+          <div>{{ user.email }}</div>
+          <div>{{ user.username }}</div>
+        </section>
+      </div>
+    </template>
+  </Layout>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import GeneralUserModel from '~/api/models/GeneralUserModel'
 import { navigateTo } from "#app/composables/router";
+import Layout from "~/component/Layout/Layout.vue";
 
 const props = defineProps({
   users: {
@@ -38,18 +43,33 @@ const redirectToUser = async (userId: string) => {
 </script>
 
 <style>
+.general-user-card-select-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+.general-user-card-select {
+  width: 40%;
+  padding: 8px;
+  text-align: center;
+  font-size: 16px;
+  border: 1px solid black;
+  border-radius: 5px;
+  cursor: pointer;
+}
 .general-user-card-container {
   display: flex;
   align-items: center;
-  padding: 10px;
+  width: 100%;
   border: solid 1px black;
   border-radius: 10px;
-  margin: 10px;
+  margin: 10px 0;
   cursor: pointer;
 }
 .general-user-card-img{
   height: 100px;
-  width: auto;
+  width: 100px;
   border: solid 1px black;
   margin: 10px;
 }
@@ -57,4 +77,5 @@ const redirectToUser = async (userId: string) => {
   display: flex;
   flex-direction: column;
 }
+
 </style>
--- component/UsersCard/UserInformationCard.vue
+++ component/UsersCard/UserInformationCard.vue
@@ -1,45 +1,50 @@
 <template>
-  <div v-if="user" class="general-user-card-container">
-    <img :src="user.avatar" alt="Avatar" class="general-user-card-img">
-    <section class="general-user-card-section">
-      <div>{{ user.last_name }}, {{ user.first_name }}</div>
-      <div>{{ user.username }}</div>
-      <div>{{ user.email }}</div>
-      <div>{{ user.gender }}</div>
-      <div>{{ user.phone_number }}</div>
-      <div>{{ user.date_of_birth }}</div>
-    </section>
-    <section>
-      <div>
-        <button @click="activeTab = 'Empleo'">Empleo</button>
-        <button @click="activeTab = 'Dirección'">Dirección</button>
-        <button @click="activeTab = 'Suscripción'">Suscripción</button>
+  <Layout>
+    <template #mainSlot>
+      <div v-if="user" class="general-user-card-container">
+        <img :src="user.avatar" alt="Avatar" class="general-user-card-img">
+        <section class="general-user-card-section">
+          <div>{{ user.last_name }}, {{ user.first_name }}</div>
+          <div>{{ user.username }}</div>
+          <div>{{ user.email }}</div>
+          <div>{{ user.gender }}</div>
+          <div>{{ user.phone_number }}</div>
+          <div>{{ user.date_of_birth }}</div>
+        </section>
+        <section>
+          <div>
+            <button @click="activeTab = 'Empleo'">Empleo</button>
+            <button @click="activeTab = 'Dirección'">Dirección</button>
+            <button @click="activeTab = 'Suscripción'">Suscripción</button>
+          </div>
+          <div v-if="activeTab === 'Empleo'">
+            <div>{{ user.employment.title }}</div>
+            <div>{{ user.employment.key_skill }}</div>
+          </div>
+          <div v-else-if="activeTab === 'Dirección'">
+            <div>{{ user.address.city }}</div>
+            <div>{{ user.address.street_name }}</div>
+            <div>{{ user.address.street_address }}</div>
+            <div>{{ user.address.zip_code }}</div>
+            <div>{{ user.address.state }}</div>
+            <div>{{ user.address.country }}</div>
+          </div>
+          <div v-else-if="activeTab === 'Suscripción'">
+            <div>{{ user.subscription.plan }}</div>
+            <div>{{ user.subscription.status }}</div>
+            <div>{{ user.subscription.payment_method }}</div>
+            <div>{{ user.subscription.term }}</div>
+          </div>
+        </section>
       </div>
-      <div v-if="activeTab === 'Empleo'">
-        <div>{{ user.employment.title }}</div>
-        <div>{{ user.employment.key_skill }}</div>
-      </div>
-      <div v-else-if="activeTab === 'Dirección'">
-        <div>{{ user.address.city }}</div>
-        <div>{{ user.address.street_name }}</div>
-        <div>{{ user.address.street_address }}</div>
-        <div>{{ user.address.zip_code }}</div>
-        <div>{{ user.address.state }}</div>
-        <div>{{ user.address.country }}</div>
-      </div>
-      <div v-else-if="activeTab === 'Suscripción'">
-        <div>{{ user.subscription.plan }}</div>
-        <div>{{ user.subscription.status }}</div>
-        <div>{{ user.subscription.payment_method }}</div>
-        <div>{{ user.subscription.term }}</div>
-      </div>
-    </section>
-  </div>
+    </template>
+  </Layout>
 </template>
 
 <script setup lang="ts">
 import GeneralUserModel from '~/api/models/GeneralUserModel'
 import { defineProps, ref } from 'vue'
+import Layout from "~/component/Layout/Layout.vue";
 
 defineProps({
   user: {
@@ -55,15 +60,15 @@ const activeTab = ref<string>('Empleo')
 .general-user-card-container {
   display: flex;
   align-items: center;
-  padding: 10px;
+  width: 100%;
   border: solid 1px black;
   border-radius: 10px;
-  margin: 10px;
+  margin: 10px 0;
   cursor: pointer;
 }
 .general-user-card-img{
   height: 100px;
-  width: auto;
+  width: 100px;
   border: solid 1px black;
   margin: 10px;
 }
