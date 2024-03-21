f6090028948cffc9dc2d98e9b5b221f887ec3668
angarciama <angarciama@unal.edu.co>
2024-03-20 19:10:11 -0500

    [Advance] -nuxt.config: configuracion de typescript y css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
+  typescript: {
+    strict: true
+  },
+  css: ['~/assets/css/index.css'],
   runtimeConfig: {
     public: {
       apiBackUrl: process.env.API_BACK_URL ?? 'https://random-data-api.com/api/v2/users?size=10&response_type=json'
